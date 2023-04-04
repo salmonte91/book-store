4b9a81ae8811ab223bda6380b8419e1d9da60dc3
James Aules <james_aules@icloud.com>
2023-04-03 21:00:43 -0400

    BookSkow C.

---
+++ src/components/BookShow.js
@@ -0,0 +1,15 @@
+const BookShow = (props) => {
+
+    const displayBook = props.data.map((item, index) => {
+        return (
+            <BookItem item={item} key={index} />
+        )
+    })
+        return (
+            <div>
+                {displayBook}
+            </div>
+        )
+}
+
+export default BookShow
