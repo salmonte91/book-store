840e95fc0e7480add713eca2c764fea5042b89d8
James Aules <james_aules@icloud.com>
2023-04-05 20:58:44 -0400

    book item -  props use

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -7,32 +7,12 @@ import axios from 'axios';
 
 const BookItem= ({ book, volume }) => {
 
-  const [volume, setVolume] = useState({});
-  const API_KEY = process.env.REACT_APP_API_KEY
-  const API_URL = `https://www.googleapis.com/books/v1/volumes/${book.id}?key=${API_KEY}`;
-
-  useEffect(() => {
-    const getVolume = async() => {
-
-        
-        // ? This is gonna get the id of the book instead of the query that i was using on searchBar
-        // ? pending
-       
-        try {
-            const res = await axios.get(API_URL)
-            setVolume(res.data.volumeinfo);
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    getVolume()
-  }, [book.id])
-
   // there was an input tag, a buttom tag & an UL tag mapping each book
   // i remove them because the SearchBar component together with the BookShow
   // are holding those functionalities
   return (
     <div>
+        {/* {boo} */}
           <h2>{volume.title}</h2>
           {/* <img/> */}
     </div>
