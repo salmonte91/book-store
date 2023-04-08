1f4cb40d2c4924abed3c841f2794819dbb97489e
salmonte91 <stevenalmonte4@gmail.com>
2023-04-08 09:04:46 -0400

    no change

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// require('dotenv').config();
-
-// the variable inside the main branch was searchBook, although i had a similar var in the SearchBar component
-// the variable inside teh main branch was searchBook, although i had a similar var in the SearchBar component
-// so i decide to change it to BookItem
-
-// we destructured props from bookShow
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const BookItem = ({ book }) => {
-
-    //inside the return i use a ternary operator so when theres no title available it manage the error
-    //same primciple with author 
-    //use join (, ) so when a book has multiple authors, this get separate by a coma
+    
     return (
         <div>
-            <h2>{book.volumeInfo.title}</h2>
-            <p>
-                <strong>Author:</strong>{" "}
-                {book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Unknown"}
-            </p>
+          {book.map((book) => (
+            <BookItem key={book.id} book={book} />
+          ))}
         </div>
-    );
-}
+      );
+    };
 
 export default BookItem;
